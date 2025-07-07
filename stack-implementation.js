b89295970b10e2e507d53e199a709fba0afd934a
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-07-07 18:45:08 +0530

    Create stack-implementation.js

---
+++ stack-implementation.js
@@ -0,0 +1,50 @@
+// time complexity: O(1) for all operations
+// space complexity: O(1) for all operations
+
+class Stack {
+    constructor() {
+        this.st = [];
+    }
+    // push element on top
+    push(element) {
+        this.st.push(element);
+    }
+    // pop top element and return it
+    pop() {
+        if (this.st.length === 0) {
+            return 'st is empty';
+        } else {
+            return this.st.pop();
+        }
+    }
+    // return top element
+    peek() {
+        if (this.st.length === 0) {
+            return 'st is empty';
+        } else {
+            return this.st[this.st.length - 1];
+        }
+    }
+    // return true if empty otherwise false
+    isEmpty() {
+        return this.st.length === 0;
+    }
+    // return number of elements
+    size() {
+        return this.st.length;
+    }
+    // clear all elements
+    clear() {
+        this.st = [];
+        return 'st cleared';
+    }
+}
+const obj = new Stack();
+obj.push(5);
+obj.push(13);
+obj.push(8);
+console.log(obj.pop()); // 8
+console.log(obj.peek()); // 13
+console.log(obj.isEmpty()); // false
+console.log(obj.size()); // 2
+console.log(obj.clear()); // st cleared
